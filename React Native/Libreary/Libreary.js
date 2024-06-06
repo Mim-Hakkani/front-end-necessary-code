c1af6c41362f1076ab3d1bc6471fa5f2fa88fc0e
Mim-Hakkani <mimhakkani4g@gmail.com>
2024-06-06 13:03:47 +0600

    react native necessary libreary

---
+++ React Native/Libreary/Libreary.js
@@ -0,0 +1,29 @@
+/*************************  React Native Animation Librearies 
+
+1. React Native Animatable
+2. React Native Reanimated
+3. Fluid Transitions for React Navigation
+4. React Native Shared Element
+5. Pose
+6. Lottie
+7. React Native Tabbar Interaction
+8. React Spring
+9. React Native SVG Animations
+10. React Native Redash
+11. React Native GL Model View
+12. React Native Flip Card
+
+ */
+
+/*******************  8 Best React Native UI Component Libraries ********************* */
+
+/*
+1. React Native Maps
+2. NativeBase
+3. React Native Elements
+4. React Native Camera
+5. React Native Paper
+6. Bit for React Native
+7. React Native Gifted Chat
+8. React Native Snap Carousel
+*/
