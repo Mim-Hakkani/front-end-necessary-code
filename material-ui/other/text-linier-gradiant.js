7b20b6a0d74f4b897202f37234e70c94be2a494d
Mim-Hakkani <mimhakkani4g@gmail.com>
2024-11-25 12:14:34 +0600

    select and gradient

---
+++ material-ui/other/text-linier-gradiant.js
@@ -0,0 +1,11 @@
+     <Typography
+                    variant="body1"
+                    sx={{
+                        background: "linear-gradient(90.38deg, #21CDF7 2.78%, #21A4F4 99.86%)",
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                        
+                    }}
+                >
+                    1-225566
+                </Typography>
