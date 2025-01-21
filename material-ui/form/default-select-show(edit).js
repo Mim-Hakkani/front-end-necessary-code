0520470d18c22841ad371e70984c00beb29a5652
Mim-Hakkani <mimhakkani4g@gmail.com>
2025-01-21 10:44:31 +0600

    debounce is done

--- material-ui/form/default-select-show(edit).js
+++ material-ui/form/default-select-show(edit).js
@@ -52,6 +52,53 @@
     },
 
 
+/*************************** country code / any *****************************************/
+
+  <FormControl fullWidth>
+            <Select
+                labelId="demo-select-small"
+                id="demo-select-large"
+                value={country} // Selected country's ID
+                size="small"
+                fullWidth
+                required
+                name="country"
+                displayEmpty
+                onChange={(e) => setCountry(e.target.value)} // Save country ID
+                renderValue={(selected) => {
+                    if (!selected || selected === "Country") return "Country";
+                    const selectedCountry = CountryData?.countries?.edges?.find(
+                        (item) => item?.node?.id === selected // Match by ID
+                    );
+                    return selectedCountry?.node?.name || "Country"; // Show name
+                }}
+                sx={{
+                    fontSize: "13.5px",
+                    color: "#2b2b2b",
+                    input: {
+                        height: "18px",
+                    },
+                    "& .MuiSvgIcon-root": {
+                        color: "#a7a7a7",
+                        fontSize: "22px",
+                    },
+                }}
+            >
+                <MenuItem value={null} sx={optionStyle}>
+                    All Country
+                </MenuItem>
+                {CountryData?.countries?.edges?.map((item) => (
+                    <MenuItem 
+                        key={item?.node?.id} // Ensure unique key
+                        value={item?.node?.id} // Save country ID in value
+                        sx={optionStyle}
+                    >
+                        {item?.node?.name}
+                    </MenuItem>
+                ))}
+            </Select>
+         </FormControl>
+
 
 
 
